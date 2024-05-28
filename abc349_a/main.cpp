a427b76e7e9321b53363861ab6382d3a24a1756d
rotarymars <sgoto2050@gmail.com>
2024-05-28 16:05:28 +0900

    commit

---
+++ abc349_a/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+signed main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int n;
+    cin>>n;
+    int tmp=0;
+    for(;n;n--){
+        int temp;
+        cin>>temp;
+        tmp+=temp;
+    }
+    cout<<-tmp<<endl;
+    return 0;
+}
