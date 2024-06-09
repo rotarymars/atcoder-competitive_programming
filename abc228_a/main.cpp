56601ff90ca1322898829c0c2da4dba6199f2be1
rotarymars <sgoto2050@gmail.com>
2024-06-09 21:03:11 +0900

    commit

---
+++ abc228_a/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#ifdef _DEBUG
+#define DP(x) cout << #x << " = " << x << endl
+#else
+#define DP(x) ;
+#endif
+using namespace std;
+signed main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int a,b,c;
+    cin>>a>>b>>c;
+    if(b<a)b+=24;
+    if(a<=c&&c<=b)cout<<"Yes\n";
+    else cout<<"No\n";
+    return 0;
+}
