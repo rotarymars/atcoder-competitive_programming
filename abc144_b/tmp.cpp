f39cf8e275a487d274d6d217f49d7fc1f980c71b
rotarymars <s.goto2050@gmail.com>
2024-07-04 17:51:23 +0900

    commit

--- abc144_b/tmp.cpp
+++ abc144_b/tmp.cpp
@@ -1,3 +1,4 @@
+#include <iostream>
 #ifndef __ROTARYMARS__PRIME_FACTORIZE__
 #define __ROTARYMARS__PRIME_FACTORIZE__
 #include <vector>
@@ -29,3 +30,20 @@ std::vector<std::pair<T, T>> PRIME_FACTORIZE(T n) {
 }
 #else
 #endif
+#ifdef _DEBUG
+#define DP(x) cout << #x << " = " << x << endl
+#else
+#define DP(x) ;
+#endif
+using namespace std;
+signed main()
+{
+    cin.tie(nullptr);
+    ios_base::sync_with_stdio(false);
+    int n;
+    cin>>n;
+    bool ok=false;
+    for(int i=1;i<=9;i++)for(int j=1;j<=9;j++)if(i*j==n)ok=true;
+    cout<<(ok?"Yes\n":"No\n");
+    return 0;
+}
