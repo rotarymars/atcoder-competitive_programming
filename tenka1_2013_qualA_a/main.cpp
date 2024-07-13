722c11b7fcc441fe50cec5b33bd191cf61b9d0a8
rotarymars <s.goto2050@gmail.com>
2024-07-14 02:02:48 +0900

    commit

---
+++ tenka1_2013_qualA_a/main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+signed main(){
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+	int n=42;
+	while(n<130000000)n*=2;
+	cout<<n<<endl;
+	return 0;
+}
