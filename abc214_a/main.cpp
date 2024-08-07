49d841a3863ae0941953b0a1235c1f3b6cc93696
rotarymars <s.goto2050@gmail.com>
2024-08-07 14:00:55 +0900

    commit

---
+++ abc214_a/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+signed main(){
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+	int n;
+	cin>>n;
+	if(n<=125)cout<<4<<endl;
+	else if(n<=211)cout<<6<<endl;
+	else cout<<8<<endl;
+	return 0;
+}
--- download.py
+++ download.py
@@ -5,38 +5,6 @@ import os
 import re
 import sys
 LOGIN_URL = "https://atcoder.jp/login"
-def getsession():
-    session = requests.session()
-    result = session.get(LOGIN_URL)
-    soup = BeautifulSoup(result.text,"lxml")
-    csrf_token = soup.find(attrs={"name": "csrf_token"}).get("value")
-    USERPATH="../username.txt"
-    PASSWORDPATH="../password.txt"
-    if os.path.exists(USERPATH) and os.path.exists(PASSWORDPATH):
-        with open(USERPATH) as f:
-            textlist=[s.rstrip() for s in f.readlines()]
-            username=textlist[0]
-        with open(PASSWORDPATH) as f:
-            textlist=[s.rstrip() for s in f.readlines()]
-            password=textlist[0]
-    else:
-        username=input("Input your username: ")
-        password = input("Input your password: ")
-        with open(USERPATH,mode="w",newline="\r\n",encoding="utf-8") as f:
-            f.write(username)
-        with open(PASSWORDPATH,mode="w",newline="\r\n",encoding="utf-8") as f:
-            f.write(password)
-    login_info = {
-    "csrf_token": csrf_token,
-    "username": username,
-    "password": password,
-    "continue": "https://atcoder.jp"
-    }
-    result = session.post(LOGIN_URL,data=login_info)
-    if result.status_code==200:
-        return session
-    else:
-        return -1
 def returnurl():
     args=sys.argv
     if len(args)==2:
@@ -45,36 +13,8 @@ def returnurl():
     for i in range(len(nowdirectory)):
         if nowdirectory[len(nowdirectory) - 1 - i] == "_":
             return f"https://atcoder.jp/contests/{nowdirectory[0:(len(nowdirectory)-1-i)]}/tasks/{nowdirectory}"
-def savetotest(ar):
-    pathtotest = "./test"
-    newlineencoding="\n"
-    if not os.path.exists(pathtotest):
-        os.system("mkdir test")
-    for i in range(0,len(ar),2):
-        with open(f"{pathtotest}/sample-{i//2}.in",mode="w",newline=newlineencoding) as f:
-            f.write(ar[i])
-        with open(f"{pathtotest}/sample-{i//2}.out",mode="w",newline=newlineencoding) as f:
-            f.write(ar[i + 1])
-def getsample(url,session):
-    result = session.get(url)
-    soup = BeautifulSoup(result.text,"lxml")
-    rawsample = list(soup.find_all("pre"))
-    sample = []
-    for i in rawsample:
-        if i.string is None:
-            continue
-        sample.append(i.string)
-    return sample
-
 def main():
-    mysession=getsession()
-    if mysession == -1:
-        print("Failed to get session. Aborting...")
-        return 1
-    problem_url = returnurl()
-    print(problem_url)
-    sampledata = getsample(problem_url,mysession)
-    savetotest(sampledata)
+    os.system(f"oj d {returnurl()}")
 
 if __name__=="__main__":
     main()
