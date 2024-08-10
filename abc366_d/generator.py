9e53a13887de8e4d78526250a528818e21e4c13b
rotarymars <s.goto2050@gmail.com>
2024-08-10 22:45:39 +0900

    commit

---
+++ abc366_d/generator.py
@@ -0,0 +1,28 @@
+import random
+n=random.randint(100,1000)
+print(n)
+for _ in range(n**2):
+    tmplist=[]
+    for _a in range(n):
+        tmplist.append(random.randint(1,999))
+    print(*tmplist)
+q=random.randint(100,10000)
+print(q)
+for _ in range(q):
+    a=1
+    b=0
+    c=1
+    d=0
+    e=1
+    f=0
+    while a>b:
+        a=random.randint(1,n)
+        b=random.randint(1,n)
+    while c>d:
+        c=random.randint(1,n)
+        d=random.randint(1,n)
+    while e>f:
+        e=random.randint(1,n)
+        f=random.randint(1,n)
+    print(a,b,c,d,e,f)
+
