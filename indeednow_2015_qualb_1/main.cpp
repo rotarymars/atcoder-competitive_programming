ab0d70335f2e877e565779115654a87d5643b614
rotarymars <s.goto2050@gmail.com>
2024-10-05 23:10:53 +0900

    commit

--- indeednow_2015_qualb_1/main.cpp
+++ indeednow_2015_qualb_1/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+#include <iostream:
 using namespace std;
 signed main(){
 	cin.tie(nullptr);
@@ -12,27 +12,27 @@ signed main(){
 
 /*
 
-                   N~                   
+                   N                    
    MMMMMMMMMMMMMMM+O$M MMMMMMMMMMMMMMM+ 
-  :M~~~~~~~~~~~~OMOOON$M,~~~~~~~~~~~~IM 
-  :M$77~~~~~~~77IMOOOOOM7I7~~~~~~~~~I8M 
-     M7~~~~~~~IMOOOOOOOOM8 ~~~~~~~:IM   
-     M7~~~~~~~IMOOOOOO8M +~~~~~~~IMM    
-     M7~~~~~~~IMOOOOOM. ~~~~~~~DIM      
-     M7~~~~~~~IMOOOMM :~~~~~~~IMM       
-     M7~~~~~~~IMOOM :~~~~~~~NIM$N       
-    MM7~~~~~~~IMMO ~~~~~~~~IMMOOO$M     
-   7+M7~~~~~~~IM O~~~~~~~ONMOOOOOOZ$.   
- M+OOM7~~~~~~~I ~~~~~~~:IMOOOOOOOOOO$M  
- M8OOM7~~~~~~~?~~~~~~~7MMOOOOOOOOOOODM  
-  88NM7~~~~~~~~~~~~~MMMOOOOOOOOOOOOD+   
-    MM7~~~~~~~~~~~~M~~MOOOOOOOOOODM     
-     M7~~~~~~~~~~=IMOOOOOOOOOOONN       
-     M7~~~~~~~~~I?~~~MM~~~MD+~~M8~~~$   
-     M7~~~~~~~IZMD~~ZOO:~~~~~~~~~~~~.   
-     M7~~~~~~IMDO8~~NOM~~NDM~~M ~~~M    
-     M7~~~~ZNMOOM~~8OO:~?DM8~~  M~~?    
-     M7~~~IM.:ND:~~MOM~~M M~~M N~~O~    
+  :M            OMOOON$M,            IM 
+  :M$77       77IMOOOOOM7I7         I8M 
+     M7       IMOOOOOOOOM8        :IM   
+     M7       IMOOOOOO8M +      IMM    
+     M7       IMOOOOOM.        DIM      
+     M7       IMOOOMM :       IMM       
+     M7       IMOOM :       NIM$N       
+    MM7       IMMO         IMMOOO$M     
+   7+M7       IM O       ONMOOOOOOZ$.   
+ M+OOM7       I        :IMOOOOOOOOOO$M  
+ M8OOM7       ?       7MMOOOOOOOOOOODM  
+  88NM7             MMMOOOOOOOOOOOOD+   
+    MM7            M  MOOOOOOOOOODM     
+     M7          =IMOOOOOOOOOOONN       
+     M7         I?   MMM  MD+~~M8~~~$   
+     M7       IZMD  ZOO:            .   
+     M7      IMDO8  NOM  NDM  M    M    
+     M7    ZNMOOM  8OO: ?DM8    M  ?    
+     M7   IM.:ND:  MOM  M M  M N  O     
      MMMMMM    MMMMMODNM+ ?III +III     
                  M8ODM                  
                   M8O                   
