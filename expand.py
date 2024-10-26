ebfc0f4570f55b9612cb11c63da2222dc3f152a9
rotarymars <s.goto2050@gmail.com>
2024-10-26 16:17:29 +0900

    🔥 Removed boost library

--- expand.py
+++ expand.py
@@ -15,12 +15,19 @@ while True:
     newsource=[]
     for i in originalsource:
         res=re.match("#include.*<template/(.*)>.*",i)
+        boostres=re.match("#include.*<boost/(.*)>.*",i)
         if res:
             nothingtochange=False
             templatesource=[]
             with open(f"../template/{res.groups()[0]}",encoding="utf-8",newline="\n") as f:
                 templatesource=f.readlines()
             newsource=newsource+templatesource
+        # elif boostres:
+        #     nothingtochange=False
+        #     boostsource=[]
+        #     with open(f"../template/boost/{boostres.groups()[0]}",encoding="utf-8",newline="\n") as f:
+        #         boostsource=f.readlines()
+        #     newsource=newsource+boostsource
         else:
             newsource.append(i)
     if nothingtochange:
