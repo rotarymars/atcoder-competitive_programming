30598ef4be98e65d078de264fb1a01f5eb7d34be
rotarymars <s.goto2050@gmail.com>
2024-11-03 15:01:06 +0900

    ✨ New problems

---
+++ abc215_c/main.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+#include <algorithm>
+using namespace std;
+signed main(){
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+  string s;
+  int k;
+  cin>>s>>k;
+  sort(s.begin(),s.end());
+  for(int i=0;i<k-1;i++){
+    next_permutation(s.begin(),s.end());
+  }
+  cout<<s<<endl;
+	return 0;
+}
+
