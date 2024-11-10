5001e504223edbb58fff2105f371fa293b0bd550
rotarymars <s.goto2050@gmail.com>
2024-11-10 11:08:30 +0900

    ✨ New problems

---
+++ tenka1_2015_qualB_a/a.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+signed main() {
+  cin.tie(nullptr);
+  ios_base::sync_with_stdio(false);
+  int a, b, c;
+  a = b = 100;
+  c = 200;
+  for (int i = 0; i < 17; i++) {
+    int temp = a + b + c;
+    a = b;
+    b = c;
+    c = temp;
+  }
+  cout << c << endl;
+  return 0;
+}
