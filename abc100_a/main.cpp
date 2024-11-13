6ba2954353318ade09e0ca21cc633353d929ee57
rotarymars <s.goto2050@gmail.com>
2024-11-13 17:32:43 +0900

    ✨ New problems

--- abc100_a/main.cpp
+++ abc100_a/main.cpp
@@ -1,17 +1,8 @@
 #include <iostream>
-#ifdef _DEBUG
-#define DP(x) cout << #x << " = " << x << endl
-#else
-#define DP(x) ;
-#endif
 using namespace std;
-signed main()
-{
-    cin.tie(nullptr);
-    ios_base::sync_with_stdio(false);
-    int a,b;
-    cin>>a>>b;
-    if(a<=8&&b<=8)cout<<"Yay!\n";
-    else cout<<":(\n";
-    return 0;
+signed main(){
+	cin.tie(nullptr);
+	ios_base::sync_with_stdio(false);
+	return 0;
 }
+
