8b40dbf7c8f87aee48fab2d112055527c7f399ef
rotarymars <s.goto2050@gmail.com>
2024-12-08 12:46:29 +0900

    new template combinations

--- template/combinations.hpp
+++ template/combinations.hpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #ifndef __ROTARYMARS__COMBINATIONS__
 #define __ROTARYMARS__COMBINATIONS__
 template<class T>
@@ -11,12 +10,3 @@ T COMBINATIONS(T a, T b){
   return ret;
 }
 #endif
-using namespace std;
-int main() {
-  cin.tie(nullptr);
-  ios::sync_with_stdio(false);
-  long long n;
-  cin>>n;
-  cout<<COMBINATIONS(n,11ll)<<endl;
-  return 0;
-}
