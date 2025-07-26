18f116ec568666e6b5e6ee0df06edffe37232bb5
rotarymars <s.goto2050@gmail.com>
2025-07-26 18:53:16 +0900

    new problem abc255/c

---
+++ abc255/c/main.cpp
@@ -0,0 +1,26 @@
+#include <template/all.hpp>
+using namespace std;
+
+int main(){
+  long long x,a,d,n;
+  cin >> x >> a >> d >> n;
+
+  if(d<0){
+    long long fi=a+d*(n-1);
+    a=fi;
+    d*=-1;
+  }
+
+  long long st=0,fi=(n-1);
+  while(st<=fi){
+    long long te=(st+fi)/2;
+    if((a+d*te)<x){st=te+1;}
+    else{fi=te-1;}
+  }
+  long long res=8e18;
+  for(long long i=max(0ll,st-5);i<=min((n-1),st+5);i++){
+    res=min(abs(a+d*i-x),res);
+  }
+  cout << res << '\n';
+  return 0;
+}
