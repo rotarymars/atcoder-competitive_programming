cb46245227027a0bb1340ca7c8486ced4ddfeecd
rotarymars <s.goto2050@gmail.com>
2025-08-16 13:03:23 +0900

    removed template
    removed submodule

--- template/inverse_element.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __ROTARYMARS__INVERSE_ELEMENT__
-#define __ROTARYMARS__INVERSE_ELEMENT__ 1
-#include <cstdlib>
-template<class T>
-T inverse_element(T x, T mod){
-    return POWMOD(x,mod-2,mod);
-}
-#else
-#endif
